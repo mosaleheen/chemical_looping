f325c90974d39b4fd03dcb6173a4d2a29b9d6058
Saleheen <saleheen@udel.edu>
2020-02-13 17:13:02 -0500

    Scripts to calculate bulk formation energy and bulk point defects energy

---
+++ bulk_formation_energy.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from math import gcd
+import numpy as np
+import linecache
+import os
+
+e_oxygen = -9.85858254518912/2.0
+n_metal, n_oxygen = [int(item) for item in linecache.getline('CONTCAR', 7).split()]
+common_denom = gcd(n_metal, n_oxygen)
+x_metal, x_oxygen = n_metal/common_denom, n_oxygen/common_denom
+
+with open('OUTCAR', 'r') as f:
+   total_bulk_energy = float([line.split()[-1] for line in f if 'entropy=' in line][-1])
+bulk_unit_energy = total_bulk_energy / common_denom
+
+bulk_metal_atoms, = [float(item) for item in linecache.getline('metal/CONTCAR', 7).split()]
+
+dir= 'metal/OUTCAR'
+if os.path.exists(dir):
+    with open (dir, 'r') as f:
+        if 'CPU' in f.read():
+            f.seek(0)
+            bulk_metal = float([line.split()[-1] for line in f if 'entropy=' in line][-1])
+        else:
+            print('Incomplete calculation')
+            exit()
+
+bulk_form_energy = bulk_unit_energy - (x_metal/bulk_metal_atoms)*bulk_metal - x_oxygen*e_oxygen
+bulk_form_per_oxygen = bulk_form_energy / x_oxygen
+
+#print('Number of metals: {}, Number of oxygens: {}'.format(n_metal, n_oxygen))
+#print('Stoichimetric number of metal: {} and oxygen: {}'.format(x_metal, x_oxygen))
+#print('Bulk metal atoms: {}, Bulk metal energy: {}'.format(bulk_metal_atoms, bulk_metal))
+#print('Unit bulk energy: {}, Bulk formation energy: {}'.format(bulk_unit_energy, bulk_form_energy))
+
+print('Bulk formation energy per oxygen atom: {}'.format(bulk_form_per_oxygen))
---
+++ bulk_vacancy.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+import os
+## parameter definition
+# energy of an oxygen atom
+e_oxygen = -9.85858254518912/2.0
+
+# check if the calculation is finished
+with open ('OUTCAR', 'r')as f:
+    if 'CPU' in f.read():
+        f.seek(0)
+        bulk_energy = float([line.split()[-1] for line in f if 'entropy=' in line][-1])
+    else: 
+        print('Bulk calculation still running')
+        exit()
+#print('Bulk energy: {}'.format(bulk_energy))
+num_vacancy = len([i for i in os.listdir('.') if os.path.isdir(i)])
+for i in range(1, num_vacancy+1):
+    dir= 'vacancy_'+ str(i)+'/OUTCAR'
+    if os.path.exists(dir):
+        with open (dir, 'r') as f:
+            if 'CPU' in f.read():
+                f.seek(0)
+                vacancy_energy = float([line.split()[-1] for line in f if 'entropy=' in line][-1])
+                vac_form_energy = vacancy_energy + e_oxygen - bulk_energy
+                print('Vacancy energy {}: {} and formation energy: {}'.format(i, vacancy_energy, vac_form_energy))
+            else:
+                print('Vacancy {} is still running'.format(i))
+                f.seek(0)
+                vacancy_energy = float([line.split()[-1] for line in f if 'entropy=' in line][-1])                
+                vac_form_energy = vacancy_energy + e_oxygen - bulk_energy                                         
+                print('Vacancy energy {}: {} and formation energy: {}'.format(i, vacancy_energy, vac_form_energy))
