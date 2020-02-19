013caeebd4ebd41c918615039590ca368680ac5d
Mohammad Saleheen <saleheen@email.sc.edu>
2020-02-19 16:59:12 -0500

    Update comments

--- relax_convergence_adsorbate.sh
+++ relax_convergence_adsorbate.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -x
-
-sed -i "s/EDIFF=.*/EDIFF= 1E-3/g" INCAR
-sed -i "s/EDIFFG=.*/# EDIFFG= -0.05/g" INCAR
+# when the convergence is tool slow/do not get converged, trying a crazy relaxed criteria
+mkdir relaxed;
+cp {INCAR,KPOINTS,POSCAR,POTCAR,job*} relaxed/;
+sed -i "s/EDIFF=.*/EDIFF= 1E-3/g" relaxed/INCAR
+sed -i "s/EDIFFG=.*/# EDIFFG= -0.05/g" relaxed/INCAR
